9eca6062c116551fa9e16314eb01d99d5a6fb5f5
ccasner <charlottecasner@gmail.com>
2017-11-01 14:28:40 -0400

    Adds delete event feature
    - Added a delete button to the event-details component
    - Added an action for deleting an event
    - Pass the action and the event to the template and route
    - In route, persist delete to API with `.destroyRecord`

--- app/components/event-details.js
+++ app/components/event-details.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteEvent() {
+      console.log(this.get('event'))
+      return this.sendAction('deleteEvent', this.get('event'))
+    }
+  }
 });
--- app/routes/event.js
+++ app/routes/event.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
   return this.get('store').findRecord('event', params.event_id)
+},
+actions: {
+  deleteEvent(events) {
+    events.destroyRecord()
+      .then(()=> this.transitionTo('events'))
+  }
 }
 });
