88a36c4d4f8a4e5e0fbfc81e7d52c1a52b2bb847
ccasner <charlottecasner@gmail.com>
2017-11-02 12:40:15 -0400

    Fixes mistakes from last commit
    Edit form is back to working and persisting

--- app/router.js
+++ app/router.js
@@ -13,7 +13,7 @@ Router.map(function () {
   this.route('users');
   this.route('events');
   this.route('events/new');
-  this.route('event', { path: '/events/:event_id'});
+  this.route('event', { path: '/events/:event_id' });
 });
 
 export default Router;
