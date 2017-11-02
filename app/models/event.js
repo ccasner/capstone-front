7f57ba938ec21bc6218fc02f8dbb917048755017
ccasner <charlottecasner@gmail.com>
2017-11-02 15:42:13 -0400

    Edits files so that all event data is showing
    Prints date and time but time still needs to be formatted (open issue
    695).
    Adds some styling to the events index

--- app/models/event.js
+++ app/models/event.js
@@ -4,8 +4,8 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   hidden: DS.attr('boolean'),
   location: DS.attr('string'),
-  date: DS.attr('date'),
-  time: DS.attr('date'),
+  date: DS.attr('string'),
+  time: DS.attr('string'),
   seats: DS.attr('number'),
   body: DS.attr('string'),
   user: DS.belongsTo('user')
