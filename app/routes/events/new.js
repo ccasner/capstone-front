0ca9469bb766780bfa691f2d669377b6a501bd3d
ccasner <charlottecasner@gmail.com>
2017-11-06 16:44:17 -0500

    Styling

--- app/routes/events/new.js
+++ app/routes/events/new.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+
+  activate () {
+    if (!this.get('isAuthenticated')) {
+      this.transitionTo('sign-in');
+    }
+  },
   actions: {
     createEvent(events){
       console.log(events)
