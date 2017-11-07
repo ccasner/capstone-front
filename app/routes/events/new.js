05fc9e4435bb61eb462fe5973d7b8806d2019663
ccasner <charlottecasner@gmail.com>
2017-11-06 20:29:57 -0500

    Adds styling to index.hbs
    Has user facing bug with 'get started' button, needs to change to
    'create event' only once user is authenticated. Right now it always says
    get started and links to sign-up form.

--- app/routes/events/new.js
+++ app/routes/events/new.js
@@ -13,7 +13,6 @@ export default Ember.Route.extend({
   },
   actions: {
     createEvent(events){
-      console.log(events)
       let newEvent = this.get('store').createRecord('event', events)
       newEvent.save()
       this.transitionTo('events')
