027a95ebaa07d89c68a3ad9c42fcc2c0cfce03a0
ccasner <charlottecasner@gmail.com>
2017-11-06 22:08:18 -0500

    Fixes user facing bugs
    Found a lot of bugs with the editing and creating functions.
    Need to find a way to only show buttons for edit and delete if the user
    own the event.
    Changed the back end so that users can only see their own events as a
    temporary fix to this problem.
    Still need to provide messages for each action.

--- app/components/edit-form.js
+++ app/components/edit-form.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
   newEvent: {
     title: null,
     hidden: false,
@@ -18,7 +22,7 @@ export default Ember.Component.extend({
       this.set('event.time', this.get('event.time'))
       this.set('event.seats', this.get('event.seats'))
       this.set('event.body', this.get('event.body'))
-      console.log(this.get('event.body'))
+      this.set('event.user', this.get('user'))
       this.sendAction('editEvent', this.get('event'))
       // this.set('event.title', null)
       // this.set('event.location', null)
